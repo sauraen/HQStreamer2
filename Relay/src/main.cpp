17db6d2da4cbbd7d93315bf666747cfc32d5b4b7
Sauraen <sauraen@gmail.com>
2020-03-28 15:28:29 -0700

    Created relay build toolchain

---
+++ Relay/src/main.cpp
@@ -0,0 +1,11 @@
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+
+#include <iostream>
+
+using namespace juce;
+
+int main(){
+    std::cout << "He" + String(110) + " world!\n";
+    return 0;
+}
