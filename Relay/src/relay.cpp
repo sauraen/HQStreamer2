125457d1795a1c2059ea4adf65935e1724130e68
Sauraen <sauraen@gmail.com>
2020-03-28 17:27:16 -0700

    Draft of relay

--- Relay/src/relay.cpp
+++ Relay/src/relay.cpp
@@ -1,14 +1,118 @@
 #include "relay.hpp"
 
+////////////////////////////////////////////////////////////////////////////////
 
+HQS2Relay::HQS2Relay() {
+    Random.getSystemRandom().setSeedRandomly();
+    //Set the passphrase to some random garbage so people don't leave it on the default
+    passphrase = String(Random.getSystemRandom().nextInt64());
+    beginWaitingForSocket(HQS2_PORT);
+    gc.reset(new ClientGCThread(*this));
+    gc->startThread();
+    stats.reset(new HostStatsThread(*this));
+    stats->startThread();
+    std::cout << "HQS2Relay running\n";
+}
+HQS2Relay::~HQS2Relay() {
+    //
+}
 
+InterprocessConnection *HQS2Relay::createConnectionObject() {
+    std::cout << "New connection opened\n";
+    const ScopedWriteLock lock(mutex);
+    HCRelay *conn = new HCRelay(*this);
+    conns.add(conn);
+    return conn;
+}
+void HQS2Relay::SendAudioPacket(const MemoryBlock &message, String session) {
+    const ScopedReadLock lock(mutex);
+    for(int c=0; c<conns.size(); ++c){
+        if(conns[c]->mode == HCRelay::Mode::Client && conns[c]->sessionname == session){
+            conns[c]->sendMessage(message);
+        }
+    }
+}
 
+void HQS2Relay::SetPassphrase(String pass) {
+    if(pass == ""){
+        std::cout << "Won't set an empty passphrase!\n";
+        return;
+    }
+    if(pass.length() < 12){
+        std::cout << "That's a really short passphrase, but okay!\n";
+    }
+    passphrase = pass;
+}
 
+String HQS2Relay::GetSessionName(int s){
+    const ScopedReadLock lock(mutex);
+    if(s < 0 || s >= sessions.size()) return "Error";
+    return sessions[s];
+}
+void HQS2Relay::KickSession(String sessionname){
+    HCRelay *host = nullptr;
+    {
+        const ScopedReadLock lock(mutex);
+        for(int c=0; c<conns.size(); ++c){
+            if(conns[c]->mode == HCRelay::Mode::Host && conns[c]->sessionname == sessionname){
+                host = conns[c];
+                break;
+            }
+        }
+    }
+    if(!host){
+        std::cout << "There is no host for session " << sessionname << "!\n";
+        return;
+    }
+    host->disconnect();
+}
+int HQS2Relay::NumClients(String sessionname){
+    const ScopedReadLock lock(mutex);
+    int nc = 0;
+    for(int c=0; c<conns.size(); ++c){
+        if(conns[c]->mode == HCRelay::Mode::Client && conns[c]->sessionname == sessionname){
+            ++nc;
+        }
+    }
+    return nc;
+}
+
+void HQS2Relay::RunGC(){
+    const ScopedWriteLock lock(mutex);
+    for(int c=0; c<conns.size(); ++c){
+        if(!conns[c]->isConnected()){
+            std::cout << "Removed stale connection " << c << "\n";
+            conns.remove(c);
+            --c;
+        }
+    }
+}
+
+void HQS2Relay::RunStats(){
+    const ScopedReadLock lock(mutex);
+    for(int c=0; c<conns.size(); ++c){
+        if(conns[c]->mode == HCRelay::Mode::Host){
+            int nc = NumClients(conns[c]->sessionname);
+            MemoryBlock ret;
+            ret.setSize(20);
+            ret.fillWith(0);
+            int32_t* s32ptr2 = (int32_t*)ret.getData();
+            s32ptr2[0] = 20;
+            s32ptr2[1] = PACKET_TYPE_HOSTSTATS;
+            s32ptr2[2] = (int32_t)nc;
+            if(!conns[c]->sendMessage(ret)){
+                
+            }
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 HCRelay::HCRelay(HQS2Relay &p) : InterprocessConnection(false, 0x32535148), parent(p) {
-    Random.getSystemRandom().setSeedRandomly();
-    challenge = Random.getSystemRandom().nextInt64();
     mode = Mode::Invalid;
+    challenge = Random.getSystemRandom().nextInt64();
+    sessionname = "";
 }
 HCRelay::~HCRelay(){
     //
@@ -19,6 +123,16 @@ void HCRelay::connectionMade(){
 }
 void HCRelay::connectionLost(){
     std::cout << "HCRelay::connectionLost()\n";
+    if(mode == Mode::Host){
+        const ScopedReadLock lock(mutex);
+        for(int c=0; c<conns.size(); ++c){
+            if(conns[c]->mode == HCRelay::Mode::Client && conns[c]->sessionname == sessionname){
+                conns[c]->disconnect();
+            }
+        }
+    }
+    mode = Mode::Invalid;
+    sessionname = "";
 }
 void HCRelay::messageReceived(const MemoryBlock& message){
     if(message.getSize() < 20 || message.getSize() > 1000000){
@@ -72,11 +186,11 @@ void HCRelay::messageReceived(const MemoryBlock& message){
             std::cout << "Wrong passphrase!\n";
         }
     }else if(type == PACKET_TYPE_REQJOIN){
-        if(packet.getSize() != 8 + HQS2_STRLEN){
+        if(message.getSize() != 8 + HQS2_STRLEN){
             std::cout << "Bad size REQJOIN! " << String((int)message.getSize()) << "\n";
             return;
         }
-        sessionname = String(CharPointer_UTF8((char*)packet.getData() + 8), HQS2_STRLEN);
+        sessionname = String(CharPointer_UTF8((char*)message.getData() + 8), HQS2_STRLEN);
         const ScopedReadLock lock(parent.mutex);
         if(!parent.sessions.contains(sessionname)){
             s32ptr[1] = PACKET_TYPE_NAKJOIN;
@@ -86,11 +200,49 @@ void HCRelay::messageReceived(const MemoryBlock& message){
             std::cout << "New client on session " << sessionname << "\n";
             mode = Mode::Client;
         }
+    }else if(type == PACKET_TYPE_AUDIO_ZEROS 
+          || type == PACKET_TYPE_AUDIO_FLOAT32
+          || type == PACKET_TYPE_AUDIO_INT16
+          || type == PACKET_TYPE_AUDIO_DPCM){
+        if(mode != Mode::Host){
+            std::cout << "Connection other than a host is sending audio!\n";
+            return;
+        }
+        int32_t nchannels = s32ptr[2], nsamples = s32ptr[3], fs = s32ptr[4];
+        if(nchannels <= 0 || nchannels > 128 || nsamples < 16 || nsamples > 100000 || fs <= 0 || fs >= 1000000){
+            std::cout << "Bad audio params in packet!\n";
+            return;
+        }
+        if(type == PACKET_TYPE_AUDIO_ZEROS && message.getSize() != 20 ||
+                type == PACKET_TYPE_AUDIO_FLOAT32 && message.getSize() != 20+4*nsamples*nchannels ||
+                type == PACKET_TYPE_AUDIO_INT16 && message.getSize() != 20+2*nsamples*nchannels ||
+                type == PACKET_TYPE_AUDIO_DPCM && message.getSize() > 20+2*nsamples*nchannels){
+            std::cout << "Bad audio packet size " << (int)message.getSize() << " for type " << (int)type << "!\n";
+            return;
+        }
+        parent.SendAudioPacket(message, sessionname);
+        return;
     }else{
-        parent.status.PushStatus(STATUS_BADAPARAM, "Bad packet type " + String((int)type) + " received!", 30);
+        std::cout << "Bad packet type " << String((int)type) << " received!\n";
         return;
     }
     if(!sendMessage(ret)){
         std::cout << "Could not send response packet\n";
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void ClientGCThread::run(){
+    while(!threadShouldExit()){
+        parent.RunGC();
+        wait(2973);
+    }
+}
+
+void HostStatsThread::run(){
+    while(!threadShouldExit()){
+        parent.RunStats();
+        wait(1023);
     }
 }
--- Relay/src/relay.hpp
+++ Relay/src/relay.hpp
@@ -2,6 +2,8 @@
 #include "../../Source/ANI.hpp"
 
 class HCRelay;
+class ClientGCThread;
+class HostStatsThread;
 
 class HQS2Relay : public InterprocessConnectionServer {
 public:
@@ -9,20 +11,25 @@ public:
     virtual ~HQS2Relay() override;
     
     virtual InterprocessConnection *createConnectionObject() override;
-    void SendAudioPacket(const MemoryBlock &message);
+    void SendAudioPacket(const MemoryBlock &message, String session);
     
     void SetPassphrase(String pass);
     
-    int NumSessions();
+    inline int NumSessions() { return sessions.size(); }
     String GetSessionName(int s);
-    void KickSession(int s);
-    int NumClients(int s);
+    void KickSession(String sessionname);
+    int NumClients(String sessionname);
+    
+    void RunGC();
+    void RunStats();
 private:
     friend class HCRelay;
     OwnedArray<HCRelay> conns;
     StringArray sessions;
     ReadWriteLock mutex;
     String passphrase;
+    std::unique_ptr<ClientGCThread> gc;
+    std::unique_ptr<HostStatsThread> stats;
 };
 
 class HCRelay : public InterprocessConnection {
@@ -40,4 +47,24 @@ public:
 private:
     int64_t challenge;
     HQS2Relay& parent;
-}
+};
+
+class ClientGCThread : public Thread {
+public:
+    ClientGCThread(HQS2Relay &p) : Thread("ClientGCThread"), parent(p) {}
+    virtual ~ClientGCThread() override {}
+    
+    virtual void run() override;
+private:
+    HQS2Relay& parent;
+};
+
+class HostStatsThread : public Thread {
+public:
+    HostStatsThread(HQS2Relay &p) : Thread("HostStatsThread"), parent(p) {}
+    virtual ~HostStatsThread() override {}
+    
+    virtual void run() override;
+private:
+    HostStatsThread& parent;
+};
